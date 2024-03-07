1fc77840d82652dabf122f7ec2d8bb3fe2437a51
Aleks-ey <Akhukhua@yahoo.com>
2024-03-07 11:36:02 -0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  site: 'https://cabinets-and-closets.com/',
+  integrations: [tailwind(), vue(), sitemap()],
 });
